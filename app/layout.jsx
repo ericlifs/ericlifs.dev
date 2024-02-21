68d871108fd844f203abd90cbbd9b33cd560f110
Eric Lifs <lifszyc.eric@gmail.com>
2024-02-21 15:39:01 -0300

    feat: add favicon

--- app/layout.jsx
+++ app/layout.jsx
@@ -24,7 +24,7 @@ export const metadata = {
 	},
 	icons: [
 		{
-			url: "/favicon.ico",
+			url: "data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>👨🏻‍💻</text></svg>",
 			rel: "icon",
 			sizes: "any",
 			type: "image/svg+xml",
