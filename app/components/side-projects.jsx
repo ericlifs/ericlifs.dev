66ee20ebd674a232b3ffef4be0d357d7ad2e4c90
Eric Lifs <lifszyc.eric@gmail.com>
2024-02-21 13:47:50 -0300

    feat: add tooltip to side-projects tech stack

--- app/components/side-projects.jsx
+++ app/components/side-projects.jsx
@@ -19,22 +19,27 @@ export default function SideProjects() {
 						<Link
 							href={card.link}
 							target="_blank"
-							className="relative flex flex-col gap-4 md:gap-6 duration-700 group p-4 sm:p-8 lg:p-4"
+							className="relative flex flex-col gap-4 md:gap-6 duration-700 group/card p-4 sm:p-8 lg:p-4"
 						>
 							<span className="font-display text-white">{card.name}</span>
 
-							<span className="text-sm transition-all duration-150 text-zinc-200 group-hover:text-white font-display">
+							<span className="text-sm transition-all duration-150 text-zinc-200 group-hover/card:text-white font-display">
 								{card.description}
 							</span>
 
 							<div className="gap-x-5 gap-y-4 flex text-neutral-950 flex-wrap">
 								{card.stack.map((tech) => (
-									<Image
-										key={tech}
-										src={`stack/${tech}.svg`}
-										width={24}
-										height={24}
-									/>
+									<span className="group/stack relative">
+										<Image
+											key={tech}
+											src={`stack/${tech}.svg`}
+											width={24}
+											height={24}
+										/>
+										<div className="bg-black absolute left-1/2 -translate-x-1/2 -bottom-10 z-10 hidden border rounded-xl border-zinc-800 text-white text-md py-1 px-2 font-display whitespace-nowrap group-hover/stack:block triangle">
+											{tech}
+										</div>
+									</span>
 								))}
 							</div>
 						</Link>
