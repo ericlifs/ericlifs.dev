f13b2e82b2c3be3348b82c4bcb38b518259c3483
Eric Lifs <lifszyc.eric@gmail.com>
2024-02-21 15:12:07 -0300

    feat: improve contact cards component styles

--- app/components/contact-cards.jsx
+++ app/components/contact-cards.jsx
@@ -1,17 +1,18 @@
 "use client";
 
-import { MarkGithubIcon, MailIcon, LinkIcon } from "@primer/octicons-react";
+import { MarkGithubIcon, MailIcon } from "@primer/octicons-react";
 import { GlowCapture } from "@codaworks/react-glow";
 import Link from "next/link";
 
 import data from "../../data.json";
 
 import SectionTitle from "./section-title";
+import LinkedinIcon from "./linkedin-icon";
 import Card from "./card";
 
 const ICONS = {
 	mail: <MailIcon size={20} />,
-	linkedin: <LinkIcon size={20} />,
+	linkedin: <LinkedinIcon size={20} />,
 	github: <MarkGithubIcon size={20} />,
 };
 
@@ -25,17 +26,16 @@ export default function ContactCards() {
 						<Link
 							href={card.link}
 							target="_blank"
-							className="relative flex flex-col gap-4 md:gap-6 duration-700 group p-4 sm:p-8 lg:p-4"
+							className="relative flex flex-col gap-4 md:gap-6 duration-700 group p-4"
 						>
-							<span className="flex items-center space-x-4">
-								<span className="w-12 h-12 text-zinc-200 group-hover:text-white border border-border glow:border-glow glow:ring-glow rounded-full p-3">
+							<span className="flex items-center gap-x-4 flex-wrap">
+								<span className="text-zinc-200 group-hover:text-white mt-1">
 									{ICONS[card.id]}
 								</span>
-								<span className="font-display text-white">{card.label}</span>
-							</span>
 
-							<span className="whitespace-nowrap text-xl duration-150 lg:text-2xl text-zinc-200 group-hover:text-white font-display">
-								{card.value}
+								<span className="truncate text-xl duration-150 text-zinc-200 group-hover:text-white font-display">
+									{card.value}
+								</span>
 							</span>
 						</Link>
 					</Card>
